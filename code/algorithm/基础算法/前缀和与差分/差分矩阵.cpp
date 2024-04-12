0a645207ab57fa7175559b161a455994eb61eb51
nian0122 <3048586944@qq.com>
2024-04-12 17:55:16 +0800

    差分矩阵更新

--- code/algorithm/基础算法/前缀和与差分/差分矩阵.cpp
+++ code/algorithm/基础算法/前缀和与差分/差分矩阵.cpp
@@ -5,7 +5,7 @@ using namespace std;
 const int N=1e3+10;
 
 int n,m,q,x1,y1,x2,y2,c;
-int a[N][N],b[N][N];
+int s[N][N],a[N][N];
 
 int main()
 {
@@ -13,25 +13,25 @@ int main()
     for(int i=1;i<=n;i++)
         for(int j=1;j<=m;j++)
         {
-            cin>>a[i][j];
-            b[i][j]=a[i][j]-a[i][j-1]-a[i-1][j]+a[i-1][j-1];
+            cin>>s[i][j];
+            a[i][j]=s[i][j]-s[i][j-1]-s[i-1][j]+s[i-1][j-1];
         }
 
     while(q--)
     {
         cin>>x1>>y1>>x2>>y2>>c;
-        b[x1][y1]+=c;
-        b[x2+1][y1]-=c;
-        b[x1][y2+1]-=c;
-        b[x2+1][y2+1]+=c;
+        a[x1][y1]+=c;
+        a[x2+1][y1]-=c;
+        a[x1][y2+1]-=c;
+        a[x2+1][y2+1]+=c;
     }
 
     for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=m;j++)
         {
-            a[i][j]=a[i][j-1]+a[i-1][j]-a[i-1][j-1]+b[i][j];
-            cout<<a[i][j];
+            s[i][j]=s[i][j-1]+s[i-1][j]-s[i-1][j-1]+a[i][j];
+            cout<<s[i][j];
         }
         cout<<endl;
     }
